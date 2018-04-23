5da05ad4f71256575c1723f312dbb46ef15afdc8
sulai3030 <sssulai@gmail.com>
2018-04-22 23:09:13 -0400

    memorygameupdates
    memory game submission

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,27 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped + cardOne");
+var arr = cards(4);
+var Array ='queen','queen','king','king';
+var Array = "cardsInPlay"[card 0];
+var flipCard = function(cardId){
+	var cardsFlipped = cards + cascade.log("Cards cardsFlipped;")
+}var arr = (cardOne);
+var cardsInPlay.push('cardOne') if cardTwo if...Else;
+var cardsInPlay.push('cardTwo');
+var checkForMatch if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+var flipCard = function(){};
+var createBoard for (var i = 0; i < arrayName.length; i++) {}length - 1; i >= 0; i--) {
+	0; i < arrayName.length; i++) {}i]
+} add eventListener(add click);
+var createElement = img;
+var newListItem = document.createElement('img');
+var setAttribute() cardElement.setAttribute('attributeName','attributeValue'); src = ("images/back.png");
+var setAttribute() 'data-id' i;
+var alert("You found a match!");
+if === alert("Sorry try again.");
+var flipCard appendChild();
+var createBoard;
+console.log(cards[cardId].cardImage)
